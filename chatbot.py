ebcbce85b02dc14207d924fe03fdb233313f8fff
nidhimadavi13-cell <nidhimadavi13@gmail.com>
2026-06-03 16:20:49 +0530

    Create chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,12 @@
+while True:
+    user=input("You:").lower()
+    if user=="hello":
+        print("Bot:Hii!")
+    elif user=="how are you":
+            print("I am fine,thanks")
+    elif user=="bye":
+                print("Goodbye")
+                break
+    else:
+          print("I don't understand")
+           
